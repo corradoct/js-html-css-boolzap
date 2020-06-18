0a9fb2967ca43f30b8fb3f3548e1469e4162e3f9
Corrado <corradoct@hotmail.it>
2020-06-18 18:11:03 +0200

    Ho completato l'esercizio

--- js/function.js
+++ js/function.js
@@ -1,10 +1,16 @@
 // ***********************  FUNZIONI  *********************//
+
+// Funzione per aggiungere uno zero davanti ai minuti se sono inferiori a 10.
+
 function addZero(i) {
   if (i < 10) {
     i = "0" + i;
   }
   return i;
 }
+
+// Funzione per ottenere l'orario attuale.
+// Ritorna l'orario formato da ora e minuti.
 
 function time() {
   var d = new Date();
@@ -14,30 +20,59 @@ function time() {
   return completeTime;
 }
 
+// Funzione per inviare un messaggio.
+// Richiede come argomento il messaggio digitato dall'utente nel campo input.
+
 function inviaMessaggio(messaggioDigitato) {
-  var clonePTemplate = $('.template p').clone();
+  // Clono il template del messaggio
+  var clonePTemplate = $('.template .with-dropdown').clone();
+  // Aggiungo il messaggio digitato dall'utente al template
   clonePTemplate.prepend(messaggioDigitato);
-  clonePTemplate.children('span').text(time());
+  // Aggiungo l'ora attuale al messaggio
+  clonePTemplate.children('.time').text(time());
+  // Aggiungo la classe per la formattazione stilistica al messaggio
   clonePTemplate.addClass('sent');
-  $('.chat').append(clonePTemplate);
-  $('.chat').scrollTop($('.chat').height());
+  // Inserisco il messaggio completo nella finestra di chat attiva
+  $('.chat .d-flex').append(clonePTemplate);
+  // Imposto lo scroll automatico della finestra attiva per avere il focus sempre sull'ultimo messaggio inviato
+  $('.chat .d-flex').scrollTop($('.chat .d-flex').prop('scrollHeight'));
+  // Resetto la barra dell'input
   $('#writeMessage').val('');
 }
 
+// Funzione per inviare un messaggio.
+// Richiede come argomento il messaggio digitato dall'utente nel campo input.
+
 function riceviMessaggio() {
-  var clonePTemplate = $('.template p').clone();
+  // Clono il template del messaggio
+  var clonePTemplate = $('.template .with-dropdown').clone();
+  // Creo il messaggio di risposta
   var message = ('Ok');
+  // Aggiungo il messaggio al template
   clonePTemplate.prepend(message);
-  clonePTemplate.children('span').text(time());
+  // Aggiungo l'ora attuale al messaggio
+  clonePTemplate.children('.time').text(time());
+  // Aggiungo la classe per la formattazione stilistica al messaggio
   clonePTemplate.addClass('received');
-  $('.chat').append(clonePTemplate);
-  $('.chat').scrollTop($('.chat').height());
+  // Inserisco il messaggio completo nella finestra di chat attiva
+  $('.chat .d-flex').append(clonePTemplate);
+  // Imposto lo scroll automatico della finestra attiva per avere il focus sempre sull'ultimo messaggio ricevuto
+  $('.chat .d-flex').scrollTop($('.chat .d-flex').prop('scrollHeight'));
+  // Cambio l'orario dell'ultimo accesso nella barra account
   $('#contactView .details span').text('Ultimo accesso oggi alle ' + time());
 }
 
+// Funzione per clonare il contatto con cui si sta chattando e modificarne i dettagli
+// Richiede come argomenti l'ora attuale
+// Ritorna l'ggetto contatto attivo con tutti i dettagli sul contatto con cui si sta chattando
+
 function contattoAttivo(ora) {
+  // Clono il contatto attivo in chat
   var contattoAttivo = $('.contactList li.active').clone();
+  // Inserisco l'ultimo accesso con l'orario
   contattoAttivo.find('span').text('Ultimo accesso alle ' + ora);
+  // Elimino l'orario vecchio
   contattoAttivo.find('.time span').text('');
+  // Ritorno il contatto attivo con i nuovi dettagli
   return contattoAttivo;
 }
--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
 $(document).ready(
   function() {
-
+    // Recupero l'ora attuale
     var ora = time();
-
+    // Aggiungo l'ora dell'ultimo accesso ai contatti in lista
     $('.contactList .active .time span').text(ora);
-
+    // Inserisco il contatto attivo in lista nella barra superiore relativa alla sua chat
     $('#contactView').append(contattoAttivo(ora));
-
+    // Assegno l'evento click ai contanti della lista
     $('.contactList li').on('click',
       function() {
+        // Se il contatto selezionato non ha la classe attiva, la rimuovo a tutti e la aggiungo solo a quello selezionato
         if (!($(this).hasClass('active'))) {
           $('.contactList li').removeClass('active');
           $(this).addClass('active');
+          // Aggiungo l'ora effettiva
           $(this).find('.time span').text(ora);
+          // Inserisco il nuovo contatto attivo nella barra della chat al posto di quello vecchio
           $('#contactView').text('');
           $('#contactView').append(contattoAttivo(ora));
+          // Recupero l'attributo data-contact dal contatto attivo e lo collego alla rispettiva finestra di chat
+          var dataContatto = $(this).attr('data-contact');
+          var selettoreChat = '.chat div[data-chat="' + dataContatto + '"]';
+          if ($(selettoreChat).hasClass('hidden')) {
+            $('.chat').children('div').removeClass('d-flex');
+            $('.chat').children('div').addClass('hidden');
+            $(selettoreChat).removeClass('hidden');
+            $(selettoreChat).addClass('d-flex');
+          }
         }
       }
     );
 
+    // Quando c'è il focus sulla barra di scrittura del messaggio diventa visibile l'icona dell'aeroplano
+    $('#writeMessage').on('focusin',
+      function() {
+        $('#microfono').addClass('hidden');
+        $('#aeroplano').removeClass('hidden');
+      }
+    );
+
+    // Quando non c'è il focus sulla barra di scrittura del messaggio diventa visibile l'icona del microfono
+    $('#writeMessage').on('focusout',
+      function() {
+        $('#microfono').removeClass('hidden');
+        $('#aeroplano').addClass('hidden');
+      }
+    );
+
+    // Al click dell'icona aeroplano viene inviato il messaggio e dopo 1 secondo si riceve il messaggio di risposta
     $('#sent').on('click',
       function() {
         var messaggio = $('#writeMessage').val();
@@ -30,6 +59,7 @@ $(document).ready(
       }
     );
 
+    // Alla pressione del tasto invio viene inviato il messaggio e dopo 1 secondo si riceve il messaggio di risposta
     $('#writeMessage').keypress(
       function(event) {
         if (event.which === 13) {
@@ -37,20 +67,27 @@ $(document).ready(
           if (messaggio != '') {
             inviaMessaggio(messaggio);
             $('#contactView .details span').text('Sta scrivendo');
-            setTimeout(riceviMessaggio, 3000);
+            setTimeout(riceviMessaggio, 1000);
           }
         }
       }
     );
 
+    // Inserendo le lettere nella barra di ricerca contatto vengono visualizzati i contatti che includono quelle lettere
     $('#searchBar').keyup(
       function() {
+        // Seleziono tutti i contatti
         var contatti = $('.contactList li');
-        $(contatti).each(
+        // Memorizzo i dati che ha inserito l'utente e il normalizzo
+        var userSearch = $(this).val().toLowerCase();
+        // Controllo per ogni contatto se sono presenti le lettere che ha inserito l'utente
+        contatti.each(
           function() {
-            var userSearch = $('#searchBar').val().toLowerCase();
+            // Faccio la ricerca normalizzando anche i nomi dei contatti
             var contactName = $(this).find('p').text().toLowerCase();
+            // Controllo se le lettere sono incluse
             var search = contactName.includes(userSearch);
+            // Se sono incluse rimuovo la classe hidden, altrimenti la aggiungo
             if (!search) {
               $(this).addClass('hidden');
             } else {
@@ -61,9 +98,21 @@ $(document).ready(
       }
     );
 
-
-
-
-
+    // Aggiungo l'evento click alla freccia della dropdown
+    $(document).on( 'click', '.with-dropdown .arrow',
+      function() {
+        // Faccio in modo che la freccia resti visibile quando la dropdown è aperta
+        $(this).toggleClass('hidden');
+        // Apro e chiudo la dropdown selezionata
+        $(this).siblings('.dropdown').toggleClass('hidden');
+        // Chiudo tutte le altre dropdown tranne quella selezionata
+        $('.dropdown').not($(this).siblings('.dropdown')).addClass('hidden');
+        // Aggiungo l'evento click alla voce cancella messaggio
+        $(this).siblings('.dropdown').find('.delete').click(
+          function() {
+            // Cancello il messaggio relativo alla dropdown selezionata
+            $(this).parents('.with-dropdown').remove();
+        });
+    });
   }
 );
